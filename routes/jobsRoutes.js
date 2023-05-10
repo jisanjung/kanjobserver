ba01f8431693112515a217e4822eb931db13930e
Jisan Jung <jjjung13@gmail.com>
2023-05-09 21:45:09 -0400

    feature: write the logic for adding a job

--- routes/jobsRoutes.js
+++ routes/jobsRoutes.js
@@ -16,8 +16,42 @@ router.get("/", async (req, res) => {
 });
 
 // get one
+router.get("/:id", async (req, res) => {
+    const jobId = req.params.id;
+    try {
+        const job = await Jobs.findOne({ job_id: jobId });
+        res.status(200).json(job);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: `there was an issue retrieving job #${jobId}`
+        });
+    }
+});
 
 // add
+router.post("/add", async (req, res) => {
+    try {
+        const jobCount = await Jobs.count();
+        const modifiedJob = req.body;
+        if (jobCount < 1) {
+            // if this is the first job that is being added
+            modifiedJob.job_id = 1;
+        } else {
+            // there is at least one job
+            const lastInsertedJob = await Jobs.find({}).sort({_id:-1}).limit(1);
+            const lastJobId = lastInsertedJob[0].job_id;
+            modifiedJob.job_id = lastJobId + 1;
+        }
+        const job = await Jobs.create(modifiedJob);
+        res.status(200).json(job);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: "could not add job to db"
+        });
+    }
+});
 
 // update
 
